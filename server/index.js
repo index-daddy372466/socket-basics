94fb9de851aaaa7b883ceeb11872a386a1699045
kylestech95 <kylestech95@penguin>
2024-08-21 18:22:50 -0400

    feat: get curseword data

--- server/index.js
+++ server/index.js
@@ -12,9 +12,11 @@ const express = require("express"),
   initializePassport = require("./passport-config.js"),
   session = require("express-session"),
   cookieParser = require("cookie-parser");
+const fs = require("fs");
+
 const MemoryStore = require("memorystore")(session);
 const { setMaxListeners } = require("events");
-const socketIoStart = require('./socketio.js')
+const socketIoStart = require("./socketio.js");
 
 const checkAuthenticated = (req, res, next) => {
   if (req.user) {
@@ -48,23 +50,28 @@ const sessionMiddleware = session({
 });
 // middleware
 
-
-app.set('views',path.resolve(__dirname,"../client/public/views"))
-app.set('view engine','ejs')
+app.set("views", path.resolve(__dirname, "../client/public/views"));
+app.set("view engine", "ejs");
 initializePassport(passport);
 // pass static html/css
 // app.use(express.static("client/public"));
 app.use(express.json());
 app.use(cookieParser());
-setMaxListeners(20)
+setMaxListeners(20);
 app.use(sessionMiddleware);
 app.use(express.urlencoded({ extended: true }));
 app.use(passport.initialize());
 app.use(passport.session());
 io.engine.use(sessionMiddleware);
-
+// cursewords from wiki page
+let cursewords = [];
 // home (conditional)
 app.route("/").get((req, res) => {
+  // let readWiki = fs.readFileSync(
+  //   path.resolve(__dirname, "../client/public/wiki.html"),
+  //   { encoding: "utf-8" }
+  // );
+  // console.log(readWiki);
   if (req.isAuthenticated()) {
     res.redirect("/home");
   } else {
@@ -72,6 +79,73 @@ app.route("/").get((req, res) => {
   }
 });
 
+// home page GET
+// app.route('/home').get(checkAuthenticated, (req,res)=>{
+// res.render('home.ejs')
+// })
+app.route("/home").get((req, res) => {
+  res.render("home.ejs");
+});
+
+// spawn wiki by writing file from url
+app.get("/wiki/write", async (req, res) => {
+  // testing to write a clone files from wikipedia page swear words
+  try {
+    let filename = "/wiki.html";
+    let file = await fetch(
+      "https://en.wiktionary.org/wiki/Category:English_swear_words"
+    )
+      .then((r) => r.text())
+      .then((d) => {
+        return d;
+      });
+      console.log(fs.existsSync(path.resolve(__dirname, "../client/public/" + filename)))
+    if(!fs.existsSync(path.resolve(__dirname, "../client/public/" + filename))){
+      fs.writeFileSync(
+        Buffer.from(path.resolve(__dirname, "../client/public/" + filename)),
+        file,
+        { encoding: "utf-8" }
+      );
+    }
+    res.redirect("/");
+  } catch (err) {
+    console.log(err);
+  }
+});
+// wiki page GET
+// if wiki file exists
+app.route("/wiki/clone").get((req, res) => {
+  res.sendFile(path.resolve(__dirname, "../client/public/wiki.html"));
+});
+// post curse words
+app.post("/wiki/curse", (req, res) => {
+  const { words } = req.body; // array of curse words
+  console.log(words);
+  if (words.length < 1) {
+    res.json({ words: "no words" });
+  } else {
+    if(cursewords.length < 1){
+      cursewords.push(...words);
+    }
+    console.log(cursewords)
+    cursewords=cursewords.slice(1,cursewords.length)
+    res.json({ words: cursewords.flat });
+  }
+});
+
+// get curse words
+app.get("/wiki/curse", (req, res) => {
+  res.json({ words: cursewords.length < 1 ? "nothing here" : cursewords.flat() });
+});
+
+// chat page GET
+app.route("/chat").get(checkAuthenticated, (req, res) => {
+  res.sendFile(path.resolve(__dirname, "../client/public/chat.html"));
+});
+// login page
+app.route("/login").get(checkNotAuthenticated, (req, res) => {
+  res.sendFile(path.resolve(__dirname, "../client/public/index.html"));
+});
 // login attempt
 app.route("/login-attempt").post(
   passport.authenticate("local", {
@@ -80,59 +154,55 @@ app.route("/login-attempt").post(
   })
 );
 
-// login page
-app.route("/login").get(checkNotAuthenticated, (req, res) => {
-  res.sendFile(path.resolve(__dirname, "../client/public/index.html"));
-});
-
-// home page GET
-// app.route('/home').get(checkAuthenticated, (req,res)=>{
-// res.render('home.ejs')
-// })
-app.route('/home').get((req,res)=>{
-  res.render('home.ejs')
-  })
-// chat page GET
-app.route("/chat").get(checkAuthenticated, (req, res) => {
-  res.sendFile(path.resolve(__dirname, "../client/public/chat.html"));
-});
 
 // logout GET
 app.get("/logout", checkAuthenticated, (req, res) => {
   req.logout(() => {
     res.redirect("/");
   });
-  console.log(req.user)
+  console.log(req.user);
 });
 
-
-let rooms = []
+let rooms = [];
 // create a room
-app.post("/create/room",(req,res)=>{
-
-  const {room}=req.body
-  console.log(room)
-  try{
-    if(room && (!rooms.includes(room)||rooms.indexOf(room)==-1)){
-      rooms.push(room)
-      console.log(rooms)
-      res.json({room:rooms})
+app.post("/create/room", (req, res) => {
+  const { room } = req.body;
+  console.log(room);
+  try {
+    if (room && (!rooms.includes(room) || rooms.indexOf(room) == -1)) {
+      rooms.push(room);
+      console.log(rooms);
+      res.json({ room: rooms });
+    } else {
+      res.status(404).json({ rooms: undefined });
     }
-    else{
-      res.status(401).send('not an authorized request')
-    }
+  } catch (err) {
+    throw err;
   }
-  catch(err){
-    throw err
-  }
-})
+});
 
+// exisiting rooms
+app.get("/room/exisiting", (req, res) => {
+  let roomData = [...rooms];
+  if (rooms.length < 1) {
+    res.json({ room: "no data" });
+  } else {
+    res.json({ room: roomData });
+  }
+});
+
+app.get("/room/:room", (req, res) => {
+  console.log(req.params.room);
+  res.json({ room: req.body.room });
+});
+
+// socket io
 socketIoStart(io);
 
 // app listen
-app.listen(PORT,()=>{
-  console.log(`listening on port ${PORT}`)
-})
+app.listen(PORT, () => {
+  console.log(`listening on port ${PORT}`);
+});
 
 // server listen
 // server.listen(PORT, () => {
