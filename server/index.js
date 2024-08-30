057de25d165e97e0dd8b9c1eec64cd466cff54df
kylestech95 <kylestech95@penguin>
2024-08-30 11:01:04 -0400

    feat: messages-filter api setup

--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ const express = require("express"),
   }),
   { Server } = require("socket.io"),
   io = new Server(httpServer);
-  (AXI = !process.env._AXI_ ? 4448 : process.env._AXI_),
+(AXI = !process.env._AXI_ ? 4448 : process.env._AXI_),
   (path = require("path")),
   (cors = require("cors")),
   (passport = require("passport")),
@@ -20,11 +20,10 @@ const express = require("express"),
   ({ setMaxListeners } = require("events")),
   (socketIoStart = require("./socketio.js")),
   (docker = "http://localhost:9998"),
-  ({ createProxyMiddleware } = require("http-proxy-middleware"))
+  ({ createProxyMiddleware } = require("http-proxy-middleware"));
 let messages = {},
   activeUsers = [],
   rooms = [];
-
 
 const checkAuthenticated = (req, res, next) => {
   if (req.user) {
@@ -42,43 +41,42 @@ const checkNotAuthenticated = (req, res, next) => {
   }
 };
 
-
-const checkIcon = (req,res,next)=>{
-  if(req.user){
-    console.log(activeUsers)
-    console.log('checking icon')
-    console.log(req.user)
-    let getActive = activeUsers.filter(x=>x.id==req.user.id)
-    console.log(getActive)
-    if(getActive[0].hasOwnProperty('icon')){
+const checkIcon = (req, res, next) => {
+  if (req.user) {
+    console.log(activeUsers);
+    console.log("checking icon");
+    console.log(req.user);
+    let getActive = activeUsers.filter((x) => x.id == req.user.id);
+    console.log(getActive);
+    if (getActive[0].hasOwnProperty("icon")) {
       next();
     } else {
-      res.redirect('/char-selection')
+      res.redirect("/char-selection");
     }
   }
-    
-}
-  // check is a user already has a profile photo.
-  const checkNoIcon = (req,res,next) => {
-    // if user is not logged in and not
-    if(!req.user){
-      console.log('user and icon not found. going login')
-      res.redirect('/login')
-    }
-    // if user is logged in, but does not have an icon set
-    else if(req.user && !activeUsers.filter(u=>req.user.id===u.id)[0].icon){
-      console.log('choose a character')
-      next();
-    }
-    else {
-      console.log('user & icon is found. going home')
-      res.redirect('/home')
-    }
-  
+};
+// check is a user already has a profile photo.
+const checkNoIcon = (req, res, next) => {
+  // if user is not logged in and not
+  if (!req.user) {
+    console.log("user and icon not found. going login");
+    res.redirect("/login");
   }
+  // if user is logged in, but does not have an icon set
+  else if (
+    req.user &&
+    !activeUsers.filter((u) => req.user.id === u.id)[0].icon
+  ) {
+    console.log("choose a character");
+    next();
+  } else {
+    console.log("user & icon is found. going home");
+    res.redirect("/home");
+  }
+};
 
 const sessionMiddleware = session({
-  name: 'uniqCkie',
+  name: "uniqCkie",
   cookie: {
     maxAge: 1800000,
     secure: false,
@@ -102,7 +100,7 @@ app.use("/api/home", createProxyMiddleware({ target: docker + "/api/home" }));
 app.use("/numbers", createProxyMiddleware({ target: docker + "/api/numbers" }));
 app.set("views", path.resolve(__dirname, "../client/public/views"));
 app.set("view engine", "ejs");
-app.use(express.static(path.resolve(__dirname,'../client/public')))
+app.use(express.static(path.resolve(__dirname, "../client/public")));
 initializePassport(passport, activeUsers);
 app.use(express.json());
 app.use(cookieParser());
@@ -142,57 +140,57 @@ app.route("/").get((req, res) => {
 });
 // home page GET
 app.route("/home").get(checkAuthenticated, (req, res) => {
-  let obj = activeUsers.find(user=>user.id==req.user.id)
-  let checkNoIcon = !obj.hasOwnProperty('icon');
-  res.render("home.ejs",{
-    checkIcon:checkNoIcon
+  let obj = activeUsers.find((user) => user.id == req.user.id);
+  let checkNoIcon = !obj.hasOwnProperty("icon");
+  res.render("home.ejs", {
+    checkIcon: checkNoIcon,
   });
 });
 
 // character selection
 app.route("/char-selection").get(checkNoIcon, (req, res) => {
-  res.render('character.ejs')
+  res.render("character.ejs");
 });
 // test - post animal to a user in activeUsers
-app.route('/char/icon').post(checkNoIcon,(req,res)=>{
-  const {icon} = req.body;
-  console.log(icon)
-  try{
-    if(req.user){
+app.route("/char/icon").post(checkNoIcon, (req, res) => {
+  const { icon } = req.body;
+  console.log(icon);
+  try {
+    if (req.user) {
       // console.log(req.user.id)
-      activeUsers.find(user=>user.id==req.user.id).icon = icon
-      let curruser = activeUsers.filter(user=>user.id==req .user.id)
-      console.log(curruser)
+      activeUsers.find((user) => user.id == req.user.id).icon = icon;
+      let curruser = activeUsers.filter((user) => user.id == req.user.id);
+      console.log(curruser);
 
-      res.json({icon:icon})
+      res.json({ icon: icon });
     }
-  }
-  catch(err){
+  } catch (err) {
     throw err;
   }
-})
+});
 
 // icon picture GET
-app.route('/char/photo').get((req,res)=>{
-  if(req.user){
-    let obj = activeUsers.find(user=>user.id === req.user.id);
-    if(!obj.hasOwnProperty('icon')){
-      res.send(`Choose a character. <a href="/char-selection">Choose character</a><br>Go home. <a href="/home">Home</a>`)
+app.route("/char/photo").get((req, res) => {
+  if (req.user) {
+    let obj = activeUsers.find((user) => user.id === req.user.id);
+    if (!obj.hasOwnProperty("icon")) {
+      res.send(
+        `Choose a character. <a href="/char-selection">Choose character</a><br>Go home. <a href="/home">Home</a>`
+      );
+    } else {
+      res.json({ icon: obj.icon });
     }
-    else{
-      res.json({icon:obj.icon})
-    }
+  } else {
+    res.send(`Sign in. <a href="/login">Sign in</a>`);
   }
-  else{
-    res.send(`Sign in. <a href="/login">Sign in</a>`)
-  }
-})
+});
 // login page
 app.route("/login").get(checkNotAuthenticated, (req, res) => {
-  res.render('index.ejs')
+  res.render("index.ejs");
 });
 // login attempt
-app.route("/login-attempt").post(checkNotAuthenticated,
+app.route("/login-attempt").post(
+  checkNotAuthenticated,
   passport.authenticate("local", {
     successRedirect: "/char-selection",
     failureRedirect: "/",
@@ -274,10 +272,16 @@ app.get("/room/:room", checkAuthenticated, checkIcon, (req, res) => {
 // store messages in fake db
 app.get("/room/:room/:message", (req, res) => {
   const { room, message } = req.params;
-  var offset = Math.floor(new Date().getTimezoneOffset() / 60) * (60*60*1000);
-  let timestamp = new Date().getTime() - offset
-  let activeuser = activeUsers.find(u=>u.id==req.user.id);
-  let obj = { message: message, sender: req.user.name, icon:activeuser.icon, timestamp:timestamp };
+  var offset =
+    Math.floor(new Date().getTimezoneOffset() / 60) * (60 * 60 * 1000);
+  let timestamp = new Date().getTime() - offset;
+  let activeuser = activeUsers.find((u) => u.id == req.user.id);
+  let obj = {
+    message: message,
+    sender: req.user.name,
+    icon: activeuser.icon,
+    timestamp: timestamp,
+  };
   messages[room] = [...messages[room], obj];
   res.json({ messages: messages[room] });
   // res.redirect('/room/'+room)
@@ -285,11 +289,30 @@ app.get("/room/:room/:message", (req, res) => {
 // get messages in fake db
 app.get("/:room/sec/messages", (req, res) => {
   const { room } = req.params;
-  if (messages[room].length > 0) {
-    res.json({ messages: messages[room] });
+  const { icon, message, sender } = req.query;
+  let keys,vals,filtered
+  if (Object.values(req.query).length > 0) {
+    keys = Object.keys(req.query), vals = Object.values(req.query);
+    let msgs = messages[room];
+    // filter the search
+    filtered = msgs.filter((m,index)=> {
+      // return null
+      return keys.every((k,idx)=>(m[k]==vals[idx]))
+    })
+    console.log(keys.join(','))
+    let property = 'filter: ' + keys.join(' and ')
+    let obj = {}
+    obj[property] = filtered
+    res.json(obj)
+    
   } else {
-    res.json({ messages: "no messages" });
+    if (messages[room].length > 0) {
+      res.json({ messages: messages[room] });
+    } else {
+      res.json({ messages: "no messages" });
+    }
   }
+
   // res.redirect('/room/'+room)
 });
 
